889e12b34d14a68f4e37643264f4a8636079615a
Edgar H Salinas <edgar@my.codergv.org>
2017-10-15 16:33:14 -0700

    selection Reducer and renderDescription

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
   render() {
     const { titleStyle } = styles;
-    return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
+    const { id, title } = this.props.library;
 
-      </CardSection>
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -23,4 +42,9 @@ const styles = {
   }
 };
 
-export default ListItem;
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+
+export default connect(mapStateToProps, actions)(ListItem);
